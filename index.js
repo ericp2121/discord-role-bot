bbdffd8a32e38d50537e93b0b3dde24969aa1fce
ericp2121 <88393383+ericp2121@users.noreply.github.com>
2022-07-29 14:28:09 -0400

    Upload fille
    Upload fille

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+
+const computer_engineering_role = '732631222622224437';
+const software_engineering_role = '732631288040915006';
+const computer_science_role = '732631354864566353';
+const electrical_engineering_role = '748600139077386380';
+
+const bot = new Discord.Client();
+
+
+
+bot.on('message',(message) => {
+
+    if (message.content == '!role ceg')
+    {
+        message.member.roles.add(computer_engineering_role);
+        message.reply('You now have the computer engineering role');
+    }
+    else if (message.content == '!role seg')
+    {
+        message.member.roles.add(software_engineering_role);
+        message.reply('You now have the software engineering role');
+    }
+    else if (message.content == '!role cs')
+    {
+        message.member.roles.add(computer_science_role);
+        message.reply('You now have the computer science role');
+    }
+    else if (message.content == '!role ee')
+    {
+        message.member.roles.add(electrical_engineering_role);
+        message.reply('You now have the electrical engineering role');
+    }
+
+});
+
+bot.login('NzUyOTAyMDQxOTMxODc0NDI2.X1eYqw.DCcMBTM6irHa0adnLpPeq57T_m8');
